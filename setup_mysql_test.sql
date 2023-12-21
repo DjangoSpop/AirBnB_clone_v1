3c3f0bd938cec454524e07ca4855650b4d72c0d1
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-12-21 03:12:34 +0200

    Add database creation and user privileges in setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,11 @@
-#!/usr/bin/env mysql
-
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
+-- Create user if not exists
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';  
+-- Grant privileges to user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
+-- Grant SELECT privilege on performance_schema to user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
