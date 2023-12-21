9b17ecba845f51883ea396028fc7ecf0a49496cb
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-12-21 02:54:43 +0200

    Add support for DB storage in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
+storage_type = os.getenv("STORAGE_TYPE")
 
-storage = FileStorage()
+if storage_type == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
 
 
